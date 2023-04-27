2e6fbd328cbd7e6a93c5485b68c8e5dcc4a97849
OmarKhaled-5758 <120251217+omarkhaled-code@users.noreply.github.com>
2023-04-27 17:25:34 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const hamIcon = document.querySelector(".wrapper-hamburger");
+
+hamIcon.onclick = () => hamIcon.classList.toggle("active");
+hamIcon.addEventListener("click", () => {
+    if (hamIcon.classList.contains("active")) {
+
+    }
+})
